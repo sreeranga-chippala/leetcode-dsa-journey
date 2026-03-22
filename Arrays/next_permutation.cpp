5b09bc0f261d1517995d40bfd1adc0e4136539ba
Sree Ranganath Chippala <chippalasreeranganath@gmail.com>
2026-03-22 21:08:00 +0530

    Day 16 Next permutation

--- Arrays/next_permutation.cpp
+++ Arrays/next_permutation.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+/* it is using built-in function but is only supported to c++ not in other languages*/
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
