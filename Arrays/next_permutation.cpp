5301485adde2f0a025d51276093d4026bf8829f9
Sree Ranganath Chippala <chippalasreeranganath@gmail.com>
2026-03-22 21:06:40 +0530

    Day 16 Next permutation

---
+++ Arrays/next_permutation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
