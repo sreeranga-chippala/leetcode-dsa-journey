8b1f6962aa835739eda3dd3855d9e7ad3886b3f8
Sree Ranganath Chippala <chippalasreeranganath@gmail.com>
2026-03-30 21:20:26 +0530

    Day 24 : Review

--- Hashing/Majority_element_more_optimal.cpp
+++ Hashing/Majority_element_more_optimal.cpp
@@ -5,7 +5,7 @@ LeetCode: https://leetcode.com/problems/majority-element/
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. 
 You may assume that the majority element always exists in the array.
-s
+
 Goal:
 
 Find the element that appears more than n/2 times.
