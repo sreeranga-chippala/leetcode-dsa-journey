2cd1117b9098ceede2c66fc99e137879bc470f52
SREE RANGANATH CHIPPALA <chippalasreeranganath@gmail.com>
2026-05-30 22:05:37 +0530

    Koko eating Bananas with commented Pseudocode

--- BinarySearch/kokoEatingBananas.cpp
+++ BinarySearch/kokoEatingBananas.cpp
@@ -30,6 +30,32 @@ As speed increases:
 
 This monotonic property allows binary search.
 
+PseudoCode : 
+
+initialize low = 1
+initialize high = maximum pile size
+
+while low < high:
+
+    mid = low + (high - low) / 2
+
+    calculate total hours needed at speed = mid
+
+    if hours <= h:
+        speed works
+        try smaller speed
+        high = mid
+
+    else:
+        speed too slow
+        try larger speed
+        low = mid + 1
+
+return low
+
+
+Complexity Analyssis :
+
 Time Complexity: O(n log M)
     n = number of piles
     M = maximum pile size
