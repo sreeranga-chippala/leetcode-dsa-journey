26a5e1674fdd090aaea8aa9318fe5c6c3279f74e
SREE RANGANATH CHIPPALA <chippalasreeranganath@gmail.com>
2026-08-07 22:45:36 +0530

    Day 152 : Balanced Binary Tree with commented PseudoCode and more optimal method

--- BinaryTrees/balanced-binary-tree.cpp
+++ BinaryTrees/balanced-binary-tree.cpp
@@ -109,3 +109,21 @@ public:
         return isBalancedTree;
     }
 };
+
+// more optimal method
+
+/*
+int height(TreeNode* root){
+    if(root == nullptr) return 0;
+
+    int left = height(root->left);
+    if(left == -1) return -1;
+
+    int right = height(root->right);
+    if(right == -1) return -1;
+
+    if(abs(left - right) > 1) return -1;
+
+    return 1 + max(left, right);
+}
+*/
